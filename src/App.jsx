954f052152be2f44a373708101180f6c3b16c9e1
Shreyas <shreyastungar762@gmail.com>
2024-09-06 00:59:30 +0530

    bug fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Routers from "./routes/Routers";
-import NavBar from "./components/NavBar";
+import NavBar from "./components/Navbar";
 
 const App = () => {
   return (
